4d932028d53e7a6c5a8f0c37c760707bb2d79c5b
Tobias Brunner <jimyouare@gmail.com>
2020-10-04 16:40:30 +0200

    added error messages for password. missing a check to see if all required fields throw no error -> make button accessible

--- frontend/src/app/user-registration/user-registration.component.ts
+++ frontend/src/app/user-registration/user-registration.component.ts
@@ -17,9 +17,12 @@ export class UserRegistrationComponent implements OnInit {
   gender = '';
   telephoneNumber = '';
   address = '';
+  check = false;
+
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+
   }
 
   registration(): void {
@@ -33,5 +36,55 @@ export class UserRegistrationComponent implements OnInit {
     }
 
 
+  evaluate(userName):b{
+    if (userName.length > 0) return true
+    else return false
+  }
+
+  //check if field is empty
+  empty(o):b{
+      if (o === "") return true;
+      else return false
+    }
+
+  //password is at least 7 characters
+  passwordLength(pw):b{
+      if (pw.length >= 7) return true;
+      else return false
+    }
+
+  // Numbers
+  passwordHasNumber(pw):b{
+      let numberOfElements = 0;
+      numberOfElements = /.*[0-9].*/.test(pw) ? ++numberOfElements : numberOfElements;
+      if (numberOfElements >= 1) return true;
+      else return false
+  }
+
+  //
+  passwordContainsMixedLetters(pw):b{
+        let numberOfElementsLow = 0;
+        numberOfElementsLow = /.*[a-z].*/.test(pw) ? ++numberOfElementsLow : numberOfElementsLow;      // Lowercase letters
+         let numberOfElementsUp = 0;
+        numberOfElementsUp = /.*[A-Z].*/.test(pw) ? ++numberOfElementsUp : numberOfElementsUp;      // Uppercase letters
+        if (numberOfElementsLow > 0 && numberOfElementsUp > 0) return true;
+           else return false
+   }
+  // Special Chars
+  passwordContainsSpecialChar(pw):b{
+      var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+
+      if(format.test(pw)){
+        return true;
+      } else {
+        return false;
+      }
+
+  }
+
+  //make register button visible
+  allFieldsAreFilled():b{
+    return false
+  }
 
 }

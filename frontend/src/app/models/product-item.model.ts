6a6e76a4b0e435db13410c889ba5a5016ade1342
Tobias Brunner <jimyouare@gmail.com>
2020-10-19 23:27:24 +0200

    tried to implement product list with model based on todolist

--- frontend/src/app/models/product-item.model.ts
+++ frontend/src/app/models/product-item.model.ts
@@ -1,21 +1,22 @@
 export class ProductItem {
-constructor(
-    public productId: number;
-    public productName: string;
-    public productDescription: string;
-    public productImage: HTMLImageElement;
-    public productPrice: number;
-    public productCategory: string;
-    public productLocation: string;
-    public uploadDate: Date;
-    public sellDate: Date;
-    public approved: boolean;
-    public isService: boolean;
-    public isLend: boolean;
-    public isAvailable: boolean;
-    public userId: number;
-    public userName: String;
-    public userReview: string;
+
+  constructor(
+    public productId: number,
+    public productName: string,
+    public productDescription: string,
+    public productImage: HTMLImageElement,
+    public productPrice: number,
+    public productCategory: string,
+    public productLocation: string,
+    public uploadDate: Date,
+    public sellDate: Date,
+    public approved: boolean,
+    public isService: boolean,
+    public isLend: boolean,
+    public isAvailable: boolean,
+    public userId: number,
+    public userName: String,
+    public userReview: string,
   ) {}
 
 }
--- frontend/src/app/models/product-list.model.ts
+++ frontend/src/app/models/product-list.model.ts
@@ -1,3 +1,10 @@
+import { ProductItem } from './product-item.model';
+
 export class ProductList {
 
+  constructor(
+    public listId: number,
+    public name: string,
+    public productItems: ProductItem[]
+  ) {}
 }

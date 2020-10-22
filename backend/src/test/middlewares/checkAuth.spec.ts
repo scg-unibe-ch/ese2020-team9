150402d9042f1176d1254f437aabb2003e52e0e5
Jan Wolfensberger <jan.wolfensberger@students.unibe.ch>
2020-10-22 14:06:39 +0200

    add test files for middlewares and services

---
+++ backend/src/test/middlewares/checkAuth.spec.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+import chai, { expect } from 'chai';
+
+describe('Test checkAuth middlewares', () => {
+    describe('Test verifyToken function', () => {
+        it('should return 403 when invalid token', function(done) {
+        });
+    });
+});

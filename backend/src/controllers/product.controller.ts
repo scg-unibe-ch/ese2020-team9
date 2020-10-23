d2e5e0c1616b3145975d5e39bcedad5cb083d34e
Samuel Meier <samuel.meier@students.unibe.ch>
2020-10-23 19:50:31 +0200

    added functions to add, update, delete,  and get products

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -1,18 +1,44 @@
 import express from 'express';
 import { Router, Request, Response } from 'express';
+import { ProductService } from '../services/product.service';
+import { userInfo } from 'os';
 
 const productController: Router = express.Router();
+const productService = new ProductService();
 
 productController.post('/', (req: Request, res: Response) => {
-    res.status(200).send({message: 'POST works!'});
+    productService.create(req.body).then(() =>
+        res.status(200).send({message: 'Product Created!'}))
+        .catch(err => res.status(500).send(err));
 });
 
 productController.put('/:id', (req: Request, res: Response) => {
-    res.status(200).send({message: 'PUT works!'});
+    productService.update(parseInt(req.params.id, 10), req.body)
+    .then(() => res.status(200).send({message: 'Product Updated!'}))
+    .catch(err => res.status(500).send(err));
 });
 
 productController.delete('/:id', (req: Request, res: Response) => {
-    res.status(200).send({message: 'DELETE works!'});
+    productService.deleteProduct(parseInt(req.params.id, 10))
+    .then(() => res.send({message: 'Product Deleted!'}))
+    .catch(err => res.status(500).send(err));
 });
 
+productController.get('/', (req: Request, res: Response) => {
+        productService.getAll().then(products => res.send(products)).catch(err => res.status(500).send(err));
+    }
+); // get all products that are available
+
+productController.get('/:category', (req: Request, res: Response) => {
+    productService.getProductsOfCategory(req.params.category).then(products => res.send(products)).catch(err => res.status(500).send(err));
+}
+); // get all products that are available of a category
+
+
+productController.get('/:id', (req: Request, res: Response) => {
+        productService.getProduct(parseInt(req.params.id, 10)).then(products => res.send(products)).catch(err => res.status(500).send(err));
+    }
+); // get product by id
+
 export const ProductController: Router = productController;
+
--- backend/src/services/product.service.ts
+++ backend/src/services/product.service.ts
@@ -1,17 +1,45 @@
-import { ProductAttributes } from './../models/product.model';
+import { resolve } from 'path';
+import { Product, ProductAttributes } from './../models/product.model';
 
 export class ProductService {
 
     public create(product: ProductAttributes): Promise<ProductAttributes> {
-        return null;
+        return(Product.create(product)).then(inserted => Promise.resolve(inserted).catch(err => Promise.reject(err)));
     }
 
-    public update(product: ProductAttributes): Promise<ProductAttributes> {
-        return null;
+    public update(id: number, product: ProductAttributes): Promise<ProductAttributes> {
+        return Product.findByPk(id).then((isFound) => isFound.update(product)
+        .then(() => {
+            return Promise.resolve(isFound);
+        }).catch(err => Promise.reject(err)));
+        }
+
+
+    public getProduct(id: number): Promise<Product> {
+        return Product.findByPk(id);
     }
 
-    public delete(id: number): Promise<any> {
-        return null;
+    public getProductsOfCategory(category: string): Promise<Product[]> {
+        const { Op } = require('sequelize');
+        return Product.findAll({
+            where: {
+              [Op.and]: [
+                { category: category }
+              ]
+            }
+          });
+
     }
 
+
+    public getAll(): Promise<Product[]> {
+        return Product.findAll();
+    }
+
+    public deleteProduct(id: number): Promise<void | Product> {
+        return Product.findByPk(id)
+        .then(isFound =>  isFound.destroy()
+            .then(() => Promise.resolve(isFound))
+            .catch(err => Promise.reject(err)));
+    }
 }

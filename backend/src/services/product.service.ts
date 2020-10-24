59fa577a7c1a1f79438894c03cf4c168d6b5710e
Samuel Meier <samuel.meier@students.unibe.ch>
2020-10-24 19:42:02 +0200

    added functions to get products

--- backend/src/services/product.service.ts
+++ backend/src/services/product.service.ts
@@ -1,42 +1,58 @@
-import { resolve } from 'path';
 import { Product, ProductAttributes } from './../models/product.model';
 
 export class ProductService {
 
     public create(product: ProductAttributes): Promise<ProductAttributes> {
-        return(Product.create(product)).then(inserted => Promise.resolve(inserted).catch(err => Promise.reject(err)));
+        return(Product.create(product))
+            .then(inserted => Promise.resolve(inserted).catch(err => Promise.reject(err)));
     }
 
-    public update(id: number, product: ProductAttributes): Promise<ProductAttributes> {
-        return Product.findByPk(id).then((isFound) => isFound.update(product)
+    public update(productId: number, product: ProductAttributes): Promise<ProductAttributes> {
+        return Product.findByPk(productId).then((isFound) => isFound.update(product)
         .then(() => {
             return Promise.resolve(isFound);
         }).catch(err => Promise.reject(err)));
         }
 
 
-    public getProduct(id: number): Promise<Product> {
-        return Product.findByPk(id);
+    /*
+    public getProduct(productId: number): Promise<Product> {
+        return Product.findByPk(productId);
     }
+    */
 
     public getProductsOfCategory(category: string): Promise<Product[]> {
         const { Op } = require('sequelize');
         return Product.findAll({
             where: {
               [Op.and]: [
-                { category: category }
+                { productCategory: category }
               ]
             }
-          });
-
+        });
     }
-
 
     public getAll(): Promise<Product[]> {
         return Product.findAll();
     }
 
-    public deleteProduct(id: number): Promise<void | Product> {
+    public getAllApproved(): Promise<Product[]> {
+        return Product.findAll({
+            where: {
+                isApproved: true
+            },
+        });
+    }
+
+    public getAllUnapproved(): Promise<Product[]> {
+        return Product.findAll({
+            where: {
+                isApproved: false
+            },
+        });
+    }
+
+    public deleteProduct(id: number): Promise<Product> {
         return Product.findByPk(id)
         .then(isFound =>  isFound.destroy()
             .then(() => Promise.resolve(isFound))

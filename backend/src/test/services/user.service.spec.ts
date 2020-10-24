7fe1e7193b8b2f5fa7f8b36c49f9486b5a7cca8e
Jan Wolfensberger <jan.wolfensberger@students.unibe.ch>
2020-10-24 14:51:01 +0200

    add test sceleton for user service

---
+++ backend/src/test/services/user.service.spec.ts
@@ -0,0 +1,11 @@
+describe('UserService Tests', () => {
+    describe('Test register', () => {
+
+    });
+    describe('Test login', () => {
+
+    });
+    describe('Test getAll', () => {
+
+    });
+});

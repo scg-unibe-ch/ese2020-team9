1821b230a289546b7039a0879d97d84b0d20786c
TheCodear <64376085+TheCodear@users.noreply.github.com>
2020-11-14 13:35:54 +0100

    add method sceleton for EmailService

--- backend/src/services/email.service.ts
+++ backend/src/services/email.service.ts
@@ -1,3 +1,37 @@
+import Mail from 'nodemailer/lib/mailer';
+import nodemailer from 'nodemailer';
+
 export class EmailService {
 
+    private mailTransport: Mail;
+
+    public EmailService() {
+        this.instantiateMailer();
+    }
+
+    public sendPasswordRestorationMail(receiver: string, token: string) {
+
+    }
+
+    private instantiateMailer() {
+        this.mailTransport = nodemailer.createTransport({
+            host: 'smtp.mail.ch',
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'stor@mail.ch',
+                pass: 'r6CFPsieyCdhFnn'
+            }
+        });
+    }
+
+    // creates the mail payload for a password forgotten request
+    private createPWMailPayload() {
+        return {
+            from: 'stor@mail.ch',
+            to: '',
+            subject: 'STOR - Reset password',
+            html: ''
+        };
+    }
 }

11a7b91f3e2683d2361151e2fe5d735ff2844983
Tobias Brunner <jimyouare@gmail.com>
2020-12-04 17:47:01 +0100

    added test for user-login

--- frontend/src/app/components/user-login/user-login.component.spec.ts
+++ frontend/src/app/components/user-login/user-login.component.spec.ts
@@ -8,11 +8,16 @@ import { UserLoginComponent } from './user-login.component';
 import { Overlay } from '@angular/cdk/overlay';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
-describe('UserLoginComponent', () => {
+fdescribe('UserLoginComponent', () => {
   let component: UserLoginComponent;
   let fixture: ComponentFixture<UserLoginComponent>;
 
   const spyRouter = jasmine.createSpyObj('Router', ['navigate']);
+
+  class MockUserService {
+    isLoggedIn = true;
+    user = { userName: 'John', password: 1234};
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -32,4 +37,59 @@ describe('UserLoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should not be logged in', () => {
+    component.ngOnInit();
+    expect(component.isUserLoggedIn).toBe(false);
+  });
+
+  it('should log out', () => {
+        component.isUserLoggedIn = true;
+        expect(component.isUserLoggedIn).toBe(true);
+        component.logout();
+        expect(component.isUserLoggedIn).toBe(false);
+    });
+
+  it('should be valid email', () => {
+       let email = 'abc@abc.com';
+       expect(component.validate(email)).toBe(true);
+     });
+
+  it('should not be valid email', () => {
+     let email = '';
+     expect(component.validate(email)).toBe(false)
+     email = 'a';
+     expect(component.validate(email)).toBe(false);
+     email = 'a@';
+     expect(component.validate(email)).toBe(false);
+     email = 'a@b';
+     expect(component.validate(email)).toBe(false);
+     email = 'a@b.c';
+     expect(component.validate(email)).toBe(false);
+   });
+
+  /*
+  it('should valid log in', () => {
+      component.userLogin = 'John';
+      component.password = '1234';
+
+      console.log(component.userLogin);
+      console.log(component.password);
+      component.login();
+      expect(component.isUserLoggedIn).toBe(true);
+  });
+  */
+
+  /*
+    it('should not be valid log in', () => {
+        component.userLogin = 'John';
+        component.password = '1234';
+
+        console.log(component.userLogin);
+        console.log(component.password);
+        component.login();
+        expect(component.isUserLoggedIn).toBe(true);
+    });
+    */
+
 });

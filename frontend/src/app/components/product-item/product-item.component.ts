ec9966c843b60f4e8b5c532014c368fb6ccc88ff
basil <basil.linder@bluewin.ch>
2020-12-05 17:25:29 +0100

    diverses for checkout master

--- frontend/src/app/components/product-item/product-item.component.ts
+++ frontend/src/app/components/product-item/product-item.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {ProductItem} from '../../models/product-item.model';
+import {ActivatedRoute, Router} from "@angular/router";
+
 
 @Component({
   selector: 'app-product-item',
@@ -6,6 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-item.component.css']
 })
 export class ProductItemComponent implements OnInit {
+
+  @Input()
+  productItem: ProductItem ; 
+
+
 
   constructor() { }
 

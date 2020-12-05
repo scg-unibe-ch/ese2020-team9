137d4ed73d174f285fef5b1344d9b2c5a173002a
TheCodear <64376085+TheCodear@users.noreply.github.com>
2020-12-05 14:45:49 +0100

    add tests for snake component

--- frontend/src/app/components/snake-component/snake-component.component.spec.ts
+++ frontend/src/app/components/snake-component/snake-component.component.spec.ts
@@ -75,4 +75,77 @@ describe('SnakeComponentComponent', () => {
     expect(component.instructions).toEqual(true);
   });
 
+  it('should display countdown when play()', () => {
+    expect(component.playing).toEqual(false);
+    component.play();
+    expect(component.playing).toEqual(false);
+    expect(component.message).toEqual('3');
+  });
+
+  it('should start the game after 3.6s', () => {
+    component.play();
+    setTimeout(() => {
+      expect(component.playing).toEqual(true);
+      expect(component.message).toEqual('START');
+    }, 3600);
+  });
+
+  it('should not initialize new game when not dead', () => {
+    component.dead = false;
+    const snake = component.snake;
+    component.run();
+    expect(component.snake == snake).toEqual(true);
+  });
+
+  it('should initialize new game when dead', () => {
+    component.dead = true;
+    const snake = component.snake;
+    component.run();
+    expect(component.snake == snake).toEqual(false);
+  });
+
+  it('should return true when valid userHighscore and call isValidUserHighscore', () => {
+    component.userHighscore = 7;
+    expect(component.isValidUserHighscore()).toEqual(true);
+  });
+
+  it('should return false when userHighscore null and call isValidUserHighscore', () => {
+    component.userHighscore = null;
+    expect(component.isValidUserHighscore()).toEqual(false);
+  });
+
+  it('should return false when userHighscore NaN and call isValidUserHighscore', () => {
+    component.userHighscore = Number.NaN;
+    expect(component.isValidUserHighscore()).toEqual(false);
+  });
+
+  it('should route to home, when calling quit', () => {
+    component.quit();
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/home']);
+  });
+
+  it('should not save highscore, when user is not logged in', () => {
+    component.userLoggedIn = false;
+    component.saveHighscore();
+    expect(component.message).toEqual('Not logged in, cannot save the highscore...');
+  });
+
+  it('should not save highscore, when highscore made lower than highscore saved', () => {
+    component.userLoggedIn = true;
+    component.userHighscore = 7;
+    component.sessionHighscore = 3;
+    component.saveHighscore();
+    expect(component.message).toEqual('You cannot save a lower score...');
+  });
+
+  it('should save highscore, when highscore made higher than highscore saved and logged in', () => {
+    component.userLoggedIn = true;
+    component.userHighscore = 3;
+    component.sessionHighscore = 7;
+    component.saveHighscore();
+    setTimeout(() => {
+      expect(component.message).toEqual('Successfully saved new highscore!');
+    }, 500);
+  });
+
 });
